292d89f035f1ccf90b29711b182acf95c3711bd8
maybemia <1754704389@qq.com>
2023-10-24 10:30:22 +0800

    fix branch

--- Crypto/ecc/task.py
+++ Crypto/ecc/task.py
@@ -1,85 +1,74 @@
-#!/usr/bin/env python3
+import sys
 
+from Crypto.Util.number import *
+from hashlib import sha256
 import fastecdsa
 from fastecdsa.point import Point
 
-FLAG = "crypto{????????????????????????????????????}"
-G = fastecdsa.curve.P256.G
-assert G.x, G.y == [0x6B17D1F2E12C4247F8BCE6E563A440F277037D812DEB33A0F4A13945D898C296,
-                    0x4FE342E2FE1A7F9B8EE7EB4A7C0F9E162BCE33576B315ECECBB6406837BF51F5]
+import socketserver
+import signal
+import string
+import random
+import os
+
+with open("flag", "rb") as f:
+    flag = f.read()
 
 
-class Challenge():
-    def __init__(self):
-        self.before_input = "Welcome to my secure search engine backed by trusted certificate library!\n"
-        self.trusted_certs = {
-            'gh4t': {
-                "public_key": Point(0xE9E4EBA2737E19663E993CF62DFBA4AF71C703ACA0A01CB003845178A51B859D,
-                                    0x179DF068FC5C380641DB2661121E568BB24BF13DE8A8968EF3D98CCF84DAF4A9),
-                "curve": "secp256r1",
-                "generator": [G.x, G.y]
-            },
-            'maybeMia': {
-                "public_key": Point(0x3B827FF5E8EA151E6E51F8D0ABF08D90F571914A595891F9998A5BD49DFA3531,
-                                    0xAB61705C502CA0F7AA127DEC096B2BBDC9BD3B4281808B3740C320810888592A),
-                "curve": "secp256r1",
-                "generator": [G.x, G.y]
-            },
-            'secsome': {
-                "public_key": Point(0xDEDFC883FEEA09DE903ECCB03C756B382B2302FFA296B03E23EEDF94B9F5AF94,
-                                    0x15CEBDD07F7584DBC7B3F4DEBBA0C13ECD2D2D8B750CBF97438AF7357CEA953D),
-                "curve": "secp256r1",
-                "generator": [G.x, G.y]
-            },
-            'v3rdant': {
-                "public_key": Point(0xDEDFC883FEEA09DE903ECCB03C756B382B2302FFA296B03E23EEDF94B9F5AF94,
-                                    0x15CEBDD07F7584DBC7B3F4DEBBA0C13ECD2D2D8B750CBF97438AF7357CEA953D),
-                "curve": "secp256r1",
-                "generator": [G.x, G.y]
-            }
-        }
+class Task(socketserver.BaseRequestHandler):
+    def __init__(self, *args, **kargs):
+        super().__init__(*args, **kargs)
 
-    def search_trusted(self, Q):
-        for host, cert in self.trusted_certs.items():
-            if Q == cert['public_key']:
-                return True, host
-        return False, None
+    def _recvAll(self):
+        BUFF_SIZE = 2048
+        data = b''
+        while True:
+            part = self.request.recv(BUFF_SIZE)
+            data += part
+            if len(part) < BUFF_SIZE:
+                break
+        return data.strip()
 
-    def sign_point(self, g, d):
-        return g * d
+    def send(self, msg, newline=True):
+        try:
+            if newline:
+                msg += b'\n'
+            self.request.sendall(msg)
+        except:
+            pass
 
-    def connection_host(self, packet):
-        d = packet['private_key']
-        if abs(d) == 1:
-            return "Private key is insecure, certificate rejected."
-        packet_host = packet['host']
-        curve = packet['curve']
-        g = Point(*packet['generator'])
-        Q = self.sign_point(g, d)
-        cached, host = self.search_trusted(Q)
-        if cached:
-            return host
-        else:
-            self.trusted_certs[packet_host] = {
-                "public_key": Q,
-                "curve": "secp256r1",
-                "generator": G
-            }
-            return "Site added to trusted connections"
+    def recv(self, prompt=b'[-] '):
+        self.send(prompt, newline=False)
+        return self._recvAll()
 
-    def bing_it(self, s):
-        return f"Hey bing! Tell me about {s}"
+    def timeout_handler(self, signum, frame):
+        raise TimeoutError
 
-    #
-    # This challenge function is called on your input, which must be JSON
-    # encoded
-    #
-    def challenge(self, your_input):
-        host = self.connection_host(your_input)
-        if host == "www.bing.com":
-            return self.bing_it(FLAG)
-        else:
-            return self.bing_it(host)
+    def proof_of_work(self):
+        random.seed(os.urandom(8))
+        proof = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(20)])
+        _hexdigest = sha256(proof.encode()).hexdigest()
+        self.send(f"sha256(XXXX+{proof[4:]}) == {_hexdigest}".encode())
+        x = self.recv(prompt=b'Plz tell me XXXX: ')
+        if len(x) != 4 or sha256(x + proof[4:].encode()).hexdigest() != _hexdigest:
+            return False
+        return True
+
+    def handle(self):
+        signal.alarm(60)
+        if not self.proof_of_work():
+            self.send(b'Failed')
+            sys.exit(0)
+
+        self.send(b'Welcome to my secure search engine backed by trusted certificate library!')
 
 
-chal = Challenge()
+class ForkedServer(socketserver.ForkingMixIn, socketserver.TCPServer):
+    pass
+
+
+if __name__ == "__main__":
+    HOST, PORT = '0.0.0.0', 10002
+    server = ForkedServer((HOST, PORT), Task)
+    server.allow_reuse_address = True
+    server.serve_forever()
